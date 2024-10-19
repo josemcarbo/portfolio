0c09a143f5447acaaa4cd126ece54cadcb71b0dd
Jose Manuel Fernandez Carbo <jose.fernandez@opstalent.com>
2024-10-18 22:40:46 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+window.addEventListener("load", () => {
+  const _options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.8,
+  };
+
+  const $sections = document.querySelectorAll(".section");
+  const $menuLinks = document.querySelectorAll("a");
+  const $blurEffect = document.querySelector(".blur-effect");
+  const $cardOverlay = document.querySelectorAll(".card-overlay");
+
+  document.querySelectorAll("a").forEach((element) => {
+    element.addEventListener("click", (event) => {
+      document.querySelectorAll("a.active").forEach((element) => {
+        element.classList.remove("active");
+      });
+      event.target.classList.add("active");
+    });
+  });
+
+  document.addEventListener("mousemove", (event) => {
+    const { clientX, clientY } = event;
+
+    $blurEffect.style.background = `radial-gradient(600px at ${clientX}px ${clientY}px,
+      rgba(29, 78, 216, 0.15), transparent 80%)`;
+  });
+
+  const activateMenuLink = (id) => {
+    $menuLinks.forEach((link) => {
+      if (link.getAttribute("href") === `#${id}`) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  };
+
+  $cardOverlay.forEach((overlay) => {
+    overlay.addEventListener("mouseover", (event) => {
+      console.log("mouseover: ", event.target);
+      $cardOverlay.forEach((element) => {
+        element.parentNode.classList.add("disabled");
+      });
+      event.target.parentNode.classList.remove("disabled");
+    });
+
+    overlay.addEventListener("mouseleave", (event) => {
+      console.log("mouseleave: ", event.target);
+      $cardOverlay.forEach((element) => {
+        element.parentNode.classList.remove("disabled");
+      });
+    });
+  });
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        activateMenuLink(entry.target.id);
+      }
+    });
+  }, _options);
+
+  $sections.forEach((section) => {
+    observer.observe(section);
+  });
+});
